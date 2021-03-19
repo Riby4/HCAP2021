af1d59da3b0e45773c7da0a4a37abe369f4c4aa1
Saúl Higareda <a01745956@itesm.mx>
2021-03-19 02:07:41 -0600

    convolucion

---
+++ convolucion.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def convolucion(Ioriginal,Kernel):
+    fr=len(Iorigina)-(len(Kernel)-1)
+    cr=len(Iorigina[0])-(len(Kernel[0])-1)
+    Resultado=np.zeros((fr,cr))
+    #For para recorre filas
+    for i in range(len(Resultado)):
+        #For para recorrer columnas
+        for j in range(len(Resultado[0])):
+            suma=0
+            #Hace multiplicaciones y las suma
+            for m in range(len(Kernel)):
+                for n in range(len(Kernel[0])):
+                    suma+=Kernel[m][n]*Ioriginal[m+i][n+j]
+            Resultado[i][j]=suma
+    return Resultado
+
+#Imagenes
+K=[[-1,0,1],[-1,0,1],[-1,0,1]]
+I=[[2,0,1,1,2],[3,0,0,0,2],[1,1,1,1,1],[3,1,1,1,2],[1,1,1,1,1]]
+
+#Imagenes a numpy arrays
+In=np.array(I)
+Kn=no.array(K)
+
+#Funcion de convolucion
+R=convolucion(In,Kn)
+print(R)
